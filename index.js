caa8bc6a68c6bc29c4acef4679ea9e8b22fd2fe6
Christian Bermeo <czaudiovisual@hotmail.com>
2021-04-03 22:04:05 -0400

    commit tree 0fcedca228d58cb8c7166981a123d844e43319b7

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(){
+    return ['Sally', 'Bob'];
+}
+returnFirstTwoDrivers();
+
+// const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+// const returnFirstTwoDrivers = function(){
+    
+// }
+
+
+function returnLastTwoDrivers(){
+    return ['Freddy', 'Claudia'];
+}
+returnLastTwoDrivers();
+
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+
+// function selectingDrivers (){
+//     function getDrivers (){
+//         return returnFirstTwoDrivers + returnLastTwoDrivers;
+//     }
+//     return selectingDrivers;
+// }
+
+function createFareMultiplier(fareMultiplier){
+    return fare => fare * 5;
+}
+
+// const fareMultiplier = function createFareMultiplier(){
+//     return createFareMultiplier * 5;
+// }
+
+function fareDoubler (num1){
+    return num1 * 2;
+}
+
+
+function fareTripler (num2){
+    return num2 * 3;
+}
+
+
+function selectDifferentDrivers (arrayOfDrivers, drivers) {
+    return drivers (arrayOfDrivers);
+}
